c4ab31e0eeeada93728fabb902d6b9a5f521891c
Alejandro Landini <landinialejandro@hotmail.com>
2023-04-30 20:10:02 -0300

    add components

---
+++ app/components/Button.php
@@ -0,0 +1,45 @@
+<?php
+class Button {
+    private $text;
+    private $url;
+    private $attributes;
+
+    public function __construct($text, $url, $attributes = []) {
+        $this->text = $text;
+        $this->url = $url;
+        $this->attributes = $attributes;
+    }
+
+    public function setText(string $text) {
+        $this->text = $text;
+    }
+
+    public function setUrl(string $url) {
+        $this->url = $url;
+    }
+
+    public function setAttribute(string $attribute, string $value) {
+        if (is_string($attribute) && is_string($value)) {
+            $this->attributes[$attribute] = $value;
+        } else {
+            throw new InvalidArgumentException('Both attribute and value must be valid strings');
+        }
+    }
+
+    private function renderAttribute(): string {
+        $attributeHtml = "";
+        foreach ($this->attributes as $attribute => $value) {
+            $attributeHtml .= " {$attribute}=\"{$value}\"";
+        }
+        return $attributeHtml;
+    }
+
+    public function render(): string {
+        $hrefHtml = "";
+        if (!empty($this->url)) {
+            $hrefHtml = " href=\"{$this->url}\"";
+        }
+
+        return "<a{$hrefHtml}{$this->renderAttribute()}><button>{$this->text}</button></a>";
+    }
+}
