29b8d4c7e106286a94dc2d6f64b13e1971593ad9
Alejandro Landini <landinialejandro@hotmail.com>
2024-12-03 13:40:40 -0300

    sin jwt

--- app/libraries/Lcobucci/JWT/Validation/SignedWith.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace app\libraries\Lcobucci\JWT\Validation;
-
-interface SignedWith extends Constraint
-{
-}
--- app/libraries/Lcobucci/JWT/Validation/ValidAt.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace app\libraries\Lcobucci\JWT\Validation;
-
-interface ValidAt extends Constraint
-{
-}
